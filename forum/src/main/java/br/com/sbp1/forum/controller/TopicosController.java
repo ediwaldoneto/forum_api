63b1bff9af34e54f339ea5a8818eb35bfb0e9b52
ediwaldoneto <ediwaldo.neto@outlook.com>
2020-08-19 07:23:40 -0300

    implemented update method

--- forum/src/main/java/br/com/sbp1/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/sbp1/forum/controller/TopicosController.java
@@ -1,27 +1,25 @@
 package br.com.sbp1.forum.controller;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
-import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import br.com.sbp1.forum.controller.dto.DetalharTopicoDto;
 import br.com.sbp1.forum.controller.dto.TopicoDto;
+import br.com.sbp1.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.sbp1.forum.controller.form.TopicoForm;
-import br.com.sbp1.forum.model.Curso;
 import br.com.sbp1.forum.model.Topico;
 import br.com.sbp1.forum.repository.CursoRepository;
 import br.com.sbp1.forum.repository.TopicoRepository;
@@ -63,4 +61,13 @@ public class TopicosController {
 		Topico topico = topicoRepository.getOne(id);
 		return new DetalharTopicoDto(topico);
 	}
+
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		Topico topico = form.atualizar(id, topicoRepository);
+
+		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+
 }
