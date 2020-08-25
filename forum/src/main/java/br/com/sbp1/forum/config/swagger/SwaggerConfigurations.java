e0a51ead61166205fc9b97e93185531882e4b33d
ediwaldoneto <ediwaldo.neto@outlook.com>
2020-08-25 06:44:06 -0300

    Finalizing API documentation with SpringFox Swagger

--- forum/src/main/java/br/com/sbp1/forum/config/swagger/SwaggerConfigurations.java
+++ forum/src/main/java/br/com/sbp1/forum/config/swagger/SwaggerConfigurations.java
@@ -1,11 +1,13 @@
 package br.com.sbp1.forum.config.swagger;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import br.com.sbp1.forum.model.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,7 +18,9 @@ public class SwaggerConfigurations {
 	public Docket forumApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("br.com.sbp1.forum")).paths(PathSelectors.ant("/**")).build()
-				.ignoredParameterTypes(Usuario.class);
+				.ignoredParameterTypes(Usuario.class).globalOperationParameters(
+						Arrays.asList(new ParameterBuilder().name("Authorization").description("Header para Token JWT")
+								.modelRef(new ModelRef("String")).parameterType("header").required(false).build()));
 
 	}
 }
